14096a2dc10acc990b1782a27f3d6fc9c0d0bac8
TheAkindele <akindelemuhyideen@gmail.com>
2024-10-07 16:15:45 +0100

    link added

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -30,7 +30,7 @@ export const LandingPage = (props: Props) => {
         </p> */}
 
         <a
-          href="https://docs.google.com/document/d/1SfsHW6eD_Ru4JZwM5EFd6tBO2A_ZMRyIInGwqpZCfOY/edit?usp=sharing"
+          href="https://docs.google.com/document/d/1mrQ3gzTim5bE1uYS7SZ3xA-izz9-gX3VhZ0yxQSlyjc/edit?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
           className="download-btn"
